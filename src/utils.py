51b948b1ce36691ba932f6b1eb6f3c6df4cb36f5
Aayush-patel-1696 <69187017+Aayush-patel-1696@users.noreply.github.com>
2024-04-05 09:27:32 -0400

    make modules for data pulling and feature creation

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+from statsmodels.tsa.stattools import coint
+
+
+
+def find_cointegrated_pairs(data):
+    n = data.shape[1]
+    score_matrix = np.zeros((n, n))
+    pvalue_matrix = np.ones((n, n))
+    keys = data.keys()
+    pairs = {}
+    for i in range(n):
+        for j in range(i+1, n):
+            S1 = data[keys[i]]
+            S2 = data[keys[j]]
+            result = coint(S1, S2)
+            score = result[0]
+            pvalue = result[1]
+            score_matrix[i, j] = score
+            pvalue_matrix[i, j] = pvalue
+            if pvalue < 0.05:
+                pairs[(keys[i], keys[j])] = result
+    return score_matrix, pvalue_matrix, pairs
+
+
+def get_top_k_pairs(pairs,k):
+    pairs_data = {key:value[1]  for (key, value) in pairs.items()}
+    pairs_data = sorted(pairs_data.items(), key=lambda x: x[1])
+    return pairs_data[0:k]
